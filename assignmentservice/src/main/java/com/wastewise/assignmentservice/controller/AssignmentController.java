0d18bcdd1727e6e3ea2f18fdb764d8edcf7bdf67
Advay Banugariya <advay.banugariya@cognizant.com>
2025-06-27 14:24:15 +0530

    Added authorization to Assignment Service modules

--- assignmentservice/src/main/java/com/wastewise/assignmentservice/controller/AssignmentController.java
+++ assignmentservice/src/main/java/com/wastewise/assignmentservice/controller/AssignmentController.java
@@ -5,6 +5,7 @@ import com.wastewise.assignmentservice.service.AssignmentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,50 +20,54 @@ public class AssignmentController {
 
 
     /**
+     * Accessed by Admin
      * Creates a new assignment.
      *
      * @param dto the assignment data to create
      * @return ResponseEntity containing the created AssignmentDTO and HTTP status 201 (Created)
      */
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<AssignmentDTO> create(@RequestBody AssignmentDTO dto) {
         log.info("Creating new assignment");
         return ResponseEntity.status(HttpStatus.CREATED).body(assignmentService.createAssignment(dto));
     }
 
-     /**
- * Retrieves an assignment by its ID.
- *
- * @param id the ID of the assignment
- * @return ResponseEntity containing the AssignmentDTO and HTTP status 200 (OK)
- */
-
+    /**
+     * Accessed by Admin
+     * Retrieves an assignment by its ID.
+     *
+     * @param id the ID of the assignment
+     * @return ResponseEntity containing the AssignmentDTO and HTTP status 200 (OK)
+     */
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<AssignmentDTO> getById(@PathVariable String id) {
         log.info("Fetching assignment with ID: {}", id);
         return ResponseEntity.ok(assignmentService.getAssignmentById(id));
     }
 
-     /**
- * Retrieves all assignments.
- *
- * @return ResponseEntity containing a list of AssignmentDTOs and HTTP status 200 (OK)
- */
+    /**
+     * Accessed by Admin
+     * Retrieves all assignments.
+     *
+     * @return ResponseEntity containing a list of AssignmentDTOs and HTTP status 200 (OK)
+     */
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<AssignmentDTO>> getAll() {
         log.info("Fetching all assignments");
         return ResponseEntity.ok(assignmentService.getAllAssignments());
     }
 
-     /**
- * Deletes an assignment by its ID.
- *
- * @param id the ID of the assignment to delete
- * @return ResponseEntity with a success message and HTTP status 200 (OK)
- */
-    
-     
+    /**
+     * Accessed by Admin
+     * Deletes an assignment by its ID.
+     *
+     * @param id the ID of the assignment to delete
+     * @return ResponseEntity with a success message and HTTP status 200 (OK)
+     */
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable String id) {
         log.info("Deleting assignment with ID: {}", id);
@@ -70,14 +75,15 @@ public class AssignmentController {
         return ResponseEntity.ok("Assignment deleted successfully.");
     }
 
-/**
-     * Updates an existing assignment.
-     *
-     * @param id  the ID of the assignment to update
-     * @param dto the updated assignment data
-     * @return ResponseEntity containing the updated AssignmentDTO and HTTP status 200 (OK)
-     */
-
+    /**
+     * Accessed by Admin
+     * Updates an existing assignment.
+     *
+     * @param id the ID of the assignment to update
+     * @param dto the updated assignment data
+     * @return ResponseEntity containing the updated AssignmentDTO and HTTP status 200 (OK)
+     */
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<AssignmentDTO> update(@PathVariable String id, @RequestBody AssignmentDTO dto) {
         log.info("Updating assignment with ID: {}", id);
@@ -85,11 +91,12 @@ public class AssignmentController {
     }
 
     /**
+     * Accessed by Admin
      * gets all the assignments assigned to route id
-     * @param routeId
-     * @return
+     * @param routeId of route
+     * @return list of assignments shared by
      */
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/by-route/{routeId}")
     public ResponseEntity<List<AssignmentDTO>> getAssignmentsByRouteId(@PathVariable String routeId) {
         log.info("Fetching assignments for route ID: {}", routeId);
