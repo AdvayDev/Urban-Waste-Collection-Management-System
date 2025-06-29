9436e8473eaa2a2763e5aa0724b4cf96d8a8a76a
Advay Banugariya <advay.banugariya@cognizant.com>
2025-06-29 10:39:01 +0530

    Added and verified Discovery server(eureka) configurations to all services

--- pickup-scheduling-main/src/main/java/com/wastewise/pickup/PickUpApplication.java
+++ pickup-scheduling-main/src/main/java/com/wastewise/pickup/PickUpApplication.java
@@ -2,8 +2,10 @@ package com.wastewise.pickup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PickUpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PickUpApplication.class, args);
--- vehicleservice/vehicleservice/src/main/java/com/wastewise/vehicleservice/VehicleserviceApplication.java
+++ vehicleservice/vehicleservice/src/main/java/com/wastewise/vehicleservice/VehicleserviceApplication.java
@@ -3,11 +3,13 @@ package com.wastewise.vehicleservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class VehicleserviceApplication {
 
     public static void main(String[] args) {
