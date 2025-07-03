78df240af13dea3913558c065e54a210ac5f64f3
Advay Banugariya <advay.banugariya@cognizant.com>
2025-07-03 11:47:17 +0530

    Updating workerClient to accomodate PickUpServiceImpl

--- pickup-scheduling-main/src/main/java/com/wastewise/pickup/client/WorkerServiceClient.java
+++ pickup-scheduling-main/src/main/java/com/wastewise/pickup/client/WorkerServiceClient.java
@@ -13,9 +13,9 @@ public interface WorkerServiceClient {
 //    @GetMapping("/wastewise/admin/workers/ids")
 //    List<WorkerDto> getAllWorkers(); //WorkerInfoDTO on worker-management module
 
-    @GetMapping("/workers/{id}")
+    @GetMapping("/internal/exists")
     Boolean checkWorkerExists(@PathVariable("id") String id);
 
-    @PatchMapping("/status/{workerId}")
-    String updateWorkerStatus(@PathVariable("workerId") String workerId, @RequestBody WorkerStatus status);
+    @PatchMapping("/internal/status")
+    String updateWorkerStatus(@RequestBody WorkerStatusUpdateDto dto);
 }
